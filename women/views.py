6a11a54d07d5d7294513133057902e415953ac89
Siarhei Zaluzhniy <levzh93@gmail.com>
2024-06-15 21:00:48 +0300

    addf: urls.py

--- women/views.py
+++ women/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+#from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse("Страница приложения Women")
+
+def categories(request):
+    return HttpResponse("<h1>Статьи по категориям</h1>")

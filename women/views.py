3ba7080a6645efd792b8c3f45c52ca617d9d0c1b
Siarhei Zaluzhniy <levzh93@gmail.com>
2024-06-15 21:15:28 +0300

    bugfix: some fix with urls

--- women/views.py
+++ women/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Страница приложения Women")
 
-def categories(request):
-    return HttpResponse("<h1>Статьи по категориям</h1>")
+def categories(request, cat_id):
+    return HttpResponse(f"<h1>Статьи по категориям</h1><p>id: {cat_id}</p>")
